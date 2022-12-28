e2a1f44401c2ee7fcc4f074f7dbc82ba880940e8
MaciejSamol <92855172+MaciejSamol@users.noreply.github.com>
2022-12-28 17:55:00 +0100

    Add profile screen
    Add database for accounts, editable name in profile screen

--- group_purchase/lib/gui/logout.dart
+++ group_purchase/lib/gui/logout.dart
@@ -1,15 +1,111 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'home_page.dart';
 
-class LogoutPage extends StatelessWidget {
+class LogoutPage extends StatefulWidget {
   final Function(User?) onSignOut;
   LogoutPage({required this.onSignOut});
 
+  @override
+  State<LogoutPage> createState() => _LogoutPageState();
+}
+
+class _LogoutPageState extends State<LogoutPage> {
+  String? name = '';
+  String? email = '';
+  String? id = '';
+  String? userNameInput = '';
+
+  Future _getDataFromDatabase() async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get()
+        .then((DocumentSnapshot documentSnapshot) async {
+      if (documentSnapshot.exists) {
+        setState(() {
+          email = '${documentSnapshot.get('email')}';
+          id = '${documentSnapshot.get('id')}';
+          name = '${documentSnapshot.get('name')}';
+        });
+      } else {
+        print('Document does not exist on the database');
+      }
+    });
+  }
+
   Future<void> logout() async {
     await FirebaseAuth.instance.signOut();
-    onSignOut(null);
+    widget.onSignOut(null);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getDataFromDatabase();
+  }
+
+  Future _updateUserName() async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .update({
+      'name': userNameInput,
+    });
+  }
+
+  _displayTextInpuitDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Uaktualnij swoje imię i nazwisko'),
+            content: TextField(
+              onChanged: (value) {
+                setState(() {
+                  userNameInput = value;
+                });
+              },
+              decoration: const InputDecoration(hintText: 'Pisz tutaj'),
+            ),
+            actions: [
+              ElevatedButton(
+                // ignore: sort_child_properties_last
+                child: const Text(
+                  'Anuluj',
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  // ignore: deprecated_member_use
+                  primary: Colors.red,
+                ),
+              ),
+              ElevatedButton(
+                // ignore: sort_child_properties_last
+                child: const Text(
+                  'Zatwierdź',
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {
+                  _updateUserName();
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (_) => MainPage()));
+                },
+                style: ElevatedButton.styleFrom(
+                  // ignore: deprecated_member_use
+                  primary: Colors.green,
+                ),
+              ),
+            ],
+          );
+        });
   }
 
   @override
@@ -19,15 +115,50 @@ class LogoutPage extends StatelessWidget {
         title: Text("Profil"),
         backgroundColor: Colors.green,
       ), // Pasek górny
-      body: ElevatedButton(
-        onPressed: () {
-          logout();
-          Navigator.pop(context);
-        },
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.green,
-        ),
-        child: Text("Wyloguj się"),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Imię i Nazwisko: ' + name!,
+                style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  _displayTextInpuitDialog(context);
+                },
+                icon: const Icon(Icons.edit),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            'Email: ' + email!,
+            style: const TextStyle(
+              fontSize: 15.0,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              logout();
+              Navigator.pop(context);
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green,
+            ),
+            child: Text("Wyloguj się"),
+          ),
+        ],
       ),
     );
   }
--- group_purchase/lib/services/database.dart
+++ group_purchase/lib/services/database.dart
@@ -1,7 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
+  final String? uid;
+  DatabaseService({this.uid});
   // collectio nreference
   final CollectionReference userCollection =
       FirebaseFirestore.instance.collection('users');
+
+  Future updateUserData(String? name, String? email, String? id) async {
+    return await userCollection.doc(uid).set({
+      'name': name,
+      'email': email,
+      'id': id,
+    });
+  }
 }
